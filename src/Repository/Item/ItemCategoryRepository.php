e5c789b976b3d0d719390e701b61bf0ddb9be7f0
Danijel Fabijan <hipnaba@gmail.com>
2020-04-09 20:18:10 +0200

    .

--- src/Repository/Item/ItemCategoryRepository.php
+++ src/Repository/Item/ItemCategoryRepository.php
@@ -1,6 +1,7 @@
 <?php
 namespace Adam\Model\Repository\Item;
 
+use Doctrine\ORM\Query;
 use Indigo\ORM\Repository\EntityRepository;
 
 /**
@@ -11,4 +12,21 @@ use Indigo\ORM\Repository\EntityRepository;
  * @link https://github.com/hipnaba/adam-model
  */
 final class ItemCategoryRepository extends EntityRepository
-{ }
+{
+    /**
+     * @return int[]
+     */
+    public function findIdsWithoutGroups(): array
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->select('c.id')
+            ->leftJoin('c.groups', 'g')
+            ->groupBy('c.id')
+            ->having($qb->expr()->eq('COUNT(g)', 0));
+
+        $query = $qb->getQuery();
+        $results = $query->getResult(Query::HYDRATE_ARRAY);
+
+        return array_map(fn (array $row) => $row['id'], $results);
+    }
+}
