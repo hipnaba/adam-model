6100af8a96961b8aec4cf81dd0ed2070ed39512e
Danijel Fabijan <hipnaba@gmail.com>
2020-04-11 01:49:13 +0200

    .

--- src/Entity/Celestial/Constellation.php
+++ src/Entity/Celestial/Constellation.php
@@ -1,9 +1,47 @@
 <?php
 namespace Adam\Model\Entity\Celestial;
 
+use Adam\Model\Entity\Character\Faction;
 use Adam\Model\Entity\Item\Item;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Class Constellation
+ *
+ * @package Adam\Model\Entity\Celestial
+ * @author Danijel Fabijan <hipnaba@gmail.com>
+ * @link https://github.com/hipnaba/adam-model
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="celestial_constellation")
+ */
 class Constellation extends Item
 {
+    /**
+     * @ORM\ManyToOne(targetEntity="\Adam\Model\Entity\Character\Faction")
+     * @ORM\JoinColumn(name="faction_id", referencedColumnName="id", nullable=true)
+     */
+    private ?Faction $faction;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="\Adam\Model\Entity\Celestial\Region", inversedBy="constellations")
+     * @ORM\JoinColumn(name="region_id", referencedColumnName="id")
+     */
+    private Region $region;
+
+    /**
+     * @return Faction|null
+     */
+    public function getFaction(): ?Faction
+    {
+        return $this->faction;
+    }
+
+    /**
+     * @return Region
+     */
+    public function getRegion(): Region
+    {
+        return $this->region;
+    }
 }
--- src/Entity/Celestial/Region.php
+++ src/Entity/Celestial/Region.php
@@ -3,6 +3,8 @@ namespace Adam\Model\Entity\Celestial;
 
 use Adam\Model\Entity\Character\Faction;
 use Adam\Model\Entity\Item\Item;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -24,10 +26,32 @@ class Region extends Item
     private ?Faction $faction;
 
     /**
+     * @ORM\OneToMany(targetEntity="\Adam\Model\Entity\Celestial\Constellation", mappedBy="region")
+     */
+    private Collection $constellations;
+
+    /**
+     * Region constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->constellations = new ArrayCollection();
+    }
+
+    /**
      * @return Faction|null
      */
     public function getFaction(): ?Faction
     {
         return $this->faction;
+    }
+
+    /**
+     * @return Constellation[]|Collection
+     */
+    public function getConstellations(): Collection
+    {
+        return $this->constellations;
     }
 }
