fd680c5f32984993597248853be684639165f457
Danijel Fabijan <hipnaba@gmail.com>
2020-04-12 01:33:28 +0200

    .

--- src/Entity/Agent/Agent.php
+++ src/Entity/Agent/Agent.php
@@ -2,6 +2,8 @@
 namespace Adam\Model\Entity\Agent;
 
 use Adam\Model\Entity\Character\Character;
+use Adam\Model\Entity\Corporation\CorporationDivision;
+use Adam\Model\Entity\Item\Item;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -23,10 +25,91 @@ class Agent extends Character
     private AgentType $agent_type;
 
     /**
+     * @ORM\ManyToOne(targetEntity="\Adam\Model\Entity\Item\Item")
+     * @ORM\JoinColumn(name="corporation_id", referencedColumnName="id")
+     */
+    private Item $corporation;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="\Adam\Model\Entity\Item\Item")
+     * @ORM\JoinColumn(name="location_id", referencedColumnName="id")
+     */
+    private Item $location;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="\Adam\Model\Entity\Corporation\CorporationDivision")
+     * @ORM\JoinColumn(name="division_id", referencedColumnName="id")
+     */
+    private CorporationDivision $division;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $level;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $quality;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $locator;
+
+    /**
      * @return AgentType
      */
     public function getAgentType(): AgentType
     {
         return $this->agent_type;
+    }
+
+    /**
+     * @return Item
+     */
+    public function getCorporation(): Item
+    {
+        return $this->corporation;
+    }
+
+    /**
+     * @return Item
+     */
+    public function getLocation(): Item
+    {
+        return $this->location;
+    }
+
+    /**
+     * @return CorporationDivision
+     */
+    public function getDivision(): CorporationDivision
+    {
+        return $this->division;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuality(): int
+    {
+        return $this->quality;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLocator(): bool
+    {
+        return $this->locator;
     }
 }
