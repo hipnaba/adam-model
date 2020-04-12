800229bfc761fe4d52b8561f6e28e7c73abb85f3
Danijel Fabijan <hipnaba@gmail.com>
2020-04-12 04:27:43 +0200

    .

--- src/Entity/Celestial/Star.php
+++ src/Entity/Celestial/Star.php
@@ -21,10 +21,62 @@ class Star extends Celestial
     private System $system;
 
     /**
+     * @ORM\Column(type="integer")
+     */
+    private int $age;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private float $luminosity;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private float $temperature;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $spectral_class;
+
+    /**
      * @return System
      */
     public function getSystem(): System
     {
         return $this->system;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLuminosity(): float
+    {
+        return $this->luminosity;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTemperature(): float
+    {
+        return $this->temperature;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSpectralClass(): string
+    {
+        return $this->spectral_class;
     }
 }
