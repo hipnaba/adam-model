65a06ac324f74aca63483a080debfd676ab22c27
Danijel Fabijan <hipnaba@gmail.com>
2020-04-12 05:29:43 +0200

    .

--- src/Entity/Item/Item.php
+++ src/Entity/Item/Item.php
@@ -22,7 +22,7 @@ use Doctrine\ORM\Mapping as ORM;
  *     "character" = "\Adam\Model\Entity\Character\Character",
  *     "faction" = "\Adam\Model\Entity\Character\CharacterFaction",
  *     "agent" = "\Adam\Model\Entity\Agent\Agent",
- *     "player" = "\Adam\Model\Entity\Character\PlayerCharacter"
+ *     "player" = "\Adam\Model\Entity\Character\PlayerCharacter",
  *     "celestial" = "\Adam\Model\Entity\Celestial\Celestial",
  *     "region" = "\Adam\Model\Entity\Celestial\Region",
  *     "constellation" = "\Adam\Model\Entity\Celestial\Constellation",
